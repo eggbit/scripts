a62e7e7eb90dec082a46be1318aaa6393aff2bb5
eggbit <gitrod@gmail.com>
2015-01-30 11:21:55 -0500

    Some apps added, others removed.

--- first.sh
+++ first.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-
 # Ask for password and keep-alive for duration of the script
 # Taken from https://gist.github.com/brandonb927/3195465
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install Homebrew
+echo "Install Homebrew..."
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Make sure everything is fine
+echo "Make sure everything is fine..."
 brew doctor || exit 1
 
-# Install Cask
+echo "Install Cask..."
 brew install caskroom/cask/brew-cask
 
-# Enable multiple cask versions
+echo "Enable multiple cask versions..."
 brew tap caskroom/versions
 
-# Software
+echo "Software..."
 brew install git
 brew install python
 brew install node
 brew install zsh
-brew install --HEAD tidy
-brew install jshint
+brew install wget
+brew install tree
 
 brew cask install firefoxdeveloperedition
 brew cask install google-chrome-dev
@@ -34,12 +33,16 @@ brew cask install macpass
 brew cask install spotify
 brew cask install iterm2
 brew cask install virtualbox
+brew cask install vagrant
 brew cask install google-drive
 brew cask install sublime-text3
 brew cask install twitterrific
 
 echo "Creating subl symlink..."
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+
+echo "Node..."
+npm install -g jshint
 
 # Set up zsh seperately.
 
