d1b322a1216db1d20f5e9f2e762d74c9968cee96
eggbit <gitrod@gmail.com>
2015-01-30 10:44:10 -0500

    Added sudo keep-alive and removed Xcode tools install (initial git request will install the tools).

--- first.sh
+++ first.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Install xcode tools
-xcode-select --install
+
+# Ask for password and keep-alive for duration of the script
+# Taken from https://gist.github.com/brandonb927/3195465
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -33,11 +36,10 @@ brew cask install iterm2
 brew cask install virtualbox
 brew cask install google-drive
 brew cask install sublime-text3
+brew cask install twitterrific
 
 echo "Creating subl symlink..."
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
-
-brew cask install twitterrific
 
 # Set up zsh seperately.
 
