f34ae62ba2fa0df681e52454c5c8660e0214b84e
eggbit <gitrod@gmail.com>
2015-03-17 16:56:25 -0400

    Directories are now in an array, added -f option to ln, and added some random directory names to avoid any posible future conflicts.

--- ram_disk.sh
+++ ram_disk.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
-SAFARI_CACHE_DIR=~/Library/Caches/com.apple.Safari
-CHROME_CACHE_DIR=~/Library/caches/Google/Chrome/default/cache
-SPOTIFY_CACHE_DIR=~/Library/caches/com.spotify.client/storage
-RAMDISK_NAME="ramdisk_$(date | md5 | cut -c 1-3)"
+RAND="uuidgen | md5 | cut -c 1-4"
+RAMDISK_NAME="ramdisk_$(eval $RAND)"
 RAMDISK_SIZE=4194304 # megabytes_you_want * 2048
+
+CACHE_PATHS=(
+    ~/library/caches/com.apple.safari
+    ~/library/caches/google/chrome/default/cache
+    ~/library/caches/com.spotify.client/storage
+    ~/library/caches/com.spotify.client/data
+)
 
 # $1 = path to move
 move_to_ram() {
-    if [[ ! -L $1 ]]; then
-        mv $1 "/Volumes/$RAMDISK_NAME/$(basename $1)"
-    else
-        unlink $1
-        mkdir "/Volumes/$RAMDISK_NAME/$(basename $1)"
+    local ramdisk_path="/Volumes/$RAMDISK_NAME/$(eval $RAND)/$(basename $1)"
+
+    mkdir -p $ramdisk_path
+
+    if [[ -d $1 ]]; then
+        mv $1 $(dirname $ramdisk_path)
     fi
 
-    ln -s "/Volumes/$RAMDISK_NAME/$(basename $1)" $1
+    ln -sf $ramdisk_path $1
 }
 
 echo "Creating 2GB ramdisk..."
 diskutil erasevolume HFS+ "$RAMDISK_NAME" `hdiutil attach -nomount ram://$RAMDISK_SIZE`
 
-echo "Moving Safari cache to ramdisk..."
-move_to_ram $SAFARI_CACHE_DIR
-
-echo "Moving Chrome cache to ramdisk..."
-move_to_ram $CHROME_CACHE_DIR
-
-echo "Moving Spotify cache to ramdisk..."
-move_to_ram $SPOTIFY_CACHE_DIR
+for i in "${CACHE_PATHS[@]}"
+do
+    echo "Moving to $RAMDISK_NAME: $i"
+    move_to_ram $i
+done
 
 echo "Done!"
-
