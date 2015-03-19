34ce0872e3df9859022728be6a5d85045d279f0e
eggbit <gitrod@gmail.com>
2015-03-19 11:50:58 -0400

    Made rand a function instead of an eval'd variable, moved functions to the top, wrapped some variables in quotes to make shellcheck happy, and removed backticks in ramdisk creation line.

--- ram_disk.sh
+++ ram_disk.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-RAND="uuidgen | md5 | cut -c 1-4"
-RAMDISK_NAME="ramdisk_$(eval $RAND)"
+rand() {
+    eval uuidgen | md5 | cut -c 1-4
+}
+
+# # $1 = path to move
+move_to_ram() {
+    local ramdisk_path
+    ramdisk_path="/Volumes/$RAMDISK_NAME/$(rand)/$(basename "$1")"
+
+    mkdir -p "$ramdisk_path"
+
+    if [[ -d "$1" ]]; then
+        mv "$1" "$(dirname "$ramdisk_path")"
+    fi
+
+    ln -sf "$ramdisk_path" "$1"
+}
+
+RAMDISK_NAME="ramdisk_$(rand)"
 RAMDISK_SIZE=4194304 # megabytes_you_want * 2048
 
 CACHE_PATHS=(
@@ -11,26 +28,13 @@ CACHE_PATHS=(
     ~/library/caches/com.spotify.client/data
 )
 
-# $1 = path to move
-move_to_ram() {
-    local ramdisk_path="/Volumes/$RAMDISK_NAME/$(eval $RAND)/$(basename $1)"
-
-    mkdir -p $ramdisk_path
-
-    if [[ -d $1 ]]; then
-        mv $1 $(dirname $ramdisk_path)
-    fi
-
-    ln -sf $ramdisk_path $1
-}
-
-echo "Creating 2GB ramdisk..."
-diskutil erasevolume HFS+ "$RAMDISK_NAME" `hdiutil attach -nomount ram://$RAMDISK_SIZE`
+# echo "Creating 2GB ramdisk..."
+diskutil erasevolume HFS+ "$RAMDISK_NAME" $(hdiutil attach -nomount ram://$RAMDISK_SIZE)
 
 for i in "${CACHE_PATHS[@]}"
 do
     echo "Moving to $RAMDISK_NAME: $i"
-    move_to_ram $i
+    move_to_ram "$i"
 done
 
 echo "Done!"
